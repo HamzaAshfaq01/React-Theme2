8a14e84b6afda658599d14c5cba9900ba85f7b72
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-08-30 13:49:29 +0500

    Updates

---
+++ precache-manifest.d9806561827adaa6f25d223edeaa910b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee1733bc6e9598d0c25a91cc615137e",
+    "url": "/React-Theme2/index.html"
+  },
+  {
+    "revision": "e2cd24e12aa6d16dea54",
+    "url": "/React-Theme2/static/css/main.f159611b.chunk.css"
+  },
+  {
+    "revision": "280fca60058d004b322f",
+    "url": "/React-Theme2/static/js/2.8e376bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Theme2/static/js/2.8e376bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cd24e12aa6d16dea54",
+    "url": "/React-Theme2/static/js/main.3701dba3.chunk.js"
+  },
+  {
+    "revision": "840296adfd5d53998617",
+    "url": "/React-Theme2/static/js/runtime-main.1ebc06c3.js"
+  },
+  {
+    "revision": "8ae59f923ed74da4a7555a6969d64d20",
+    "url": "/React-Theme2/static/media/about-guitar.8ae59f92.png"
+  },
+  {
+    "revision": "23ef8835057946bece6c534b640e3fb6",
+    "url": "/React-Theme2/static/media/about-monitors.23ef8835.png"
+  },
+  {
+    "revision": "8db264760d28c6f6567e6c69b182c97c",
+    "url": "/React-Theme2/static/media/about-waves.8db26476.png"
+  },
+  {
+    "revision": "7b119b9635b6ea1668bee3320e0d4776",
+    "url": "/React-Theme2/static/media/artist1.7b119b96.jpg"
+  },
+  {
+    "revision": "86ffc8fbf213b23dc48d7e9c7712f2f0",
+    "url": "/React-Theme2/static/media/artist2.86ffc8fb.jpg"
+  },
+  {
+    "revision": "96f479b4017e547855f2734c65ee7c0a",
+    "url": "/React-Theme2/static/media/artist3.96f479b4.jpg"
+  },
+  {
+    "revision": "6b37d29d58fc9b6385355dfbc3efa78a",
+    "url": "/React-Theme2/static/media/artist4.6b37d29d.jpg"
+  },
+  {
+    "revision": "124a66a33b121768b89103601d19faf8",
+    "url": "/React-Theme2/static/media/bg1.124a66a3.jpg"
+  },
+  {
+    "revision": "ffa2efc1ec0d0cbe85e706d06f37fa5e",
+    "url": "/React-Theme2/static/media/footer-bg2.ffa2efc1.jpg"
+  },
+  {
+    "revision": "70d69393675a3e6c91869ac1e1f26335",
+    "url": "/React-Theme2/static/media/gallery_01.70d69393.jpg"
+  },
+  {
+    "revision": "cc4d263276e539e694ef47d2411b39f8",
+    "url": "/React-Theme2/static/media/gallery_02.cc4d2632.jpg"
+  },
+  {
+    "revision": "f8cfe71bc5b94f0d99907119af8ba33c",
+    "url": "/React-Theme2/static/media/gallery_03.f8cfe71b.jpg"
+  },
+  {
+    "revision": "60b2f719ca7bc7738446dd7eb16246be",
+    "url": "/React-Theme2/static/media/gallery_04.60b2f719.jpg"
+  },
+  {
+    "revision": "2b7dde49900500d620238c6b71847d34",
+    "url": "/React-Theme2/static/media/gallery_05.2b7dde49.jpg"
+  },
+  {
+    "revision": "ef7130254777d486484af0b65b2870e9",
+    "url": "/React-Theme2/static/media/gallery_06.ef713025.jpg"
+  },
+  {
+    "revision": "7f33668cfd85cd89e557b62107fc7dfb",
+    "url": "/React-Theme2/static/media/gallery_07.7f33668c.jpg"
+  },
+  {
+    "revision": "9338c5063a13a8cf281d364791c0f0af",
+    "url": "/React-Theme2/static/media/gallery_08.9338c506.jpg"
+  },
+  {
+    "revision": "7e6c78557c3ff8776a0130062c74e0d5",
+    "url": "/React-Theme2/static/media/gallery_09.7e6c7855.jpg"
+  },
+  {
+    "revision": "0fb12d4fc9352bec401e809e88a3b2b3",
+    "url": "/React-Theme2/static/media/gallery_10.0fb12d4f.jpg"
+  },
+  {
+    "revision": "b9a085d03e709a88f8f67d0ff04ff2eb",
+    "url": "/React-Theme2/static/media/gallery_11.b9a085d0.jpg"
+  },
+  {
+    "revision": "2e32d5a16e62e4e23e085d7515c7a176",
+    "url": "/React-Theme2/static/media/serv_01-750x525.2e32d5a1.jpg"
+  },
+  {
+    "revision": "433a3aa53712c4590cd6bb559dfd1411",
+    "url": "/React-Theme2/static/media/serv_02-750x525.433a3aa5.jpg"
+  }
+]);
